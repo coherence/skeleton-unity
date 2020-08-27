8c02cb5d1364e9e9d971461b3a351c8b0dd710d7
Erik Svedäng <erik@coherence.io>
2020-08-27 17:13:08 +0200

    Nice input logic.

--- Assets/Example_ECS/Scripts/InputSystem.cs
+++ Assets/Example_ECS/Scripts/InputSystem.cs
@@ -6,19 +6,16 @@ class InputSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        var x = 0f;
-        var y = 0f;
+        var vec = new float2();
 
-        if(UnityEngine.Input.GetKey(KeyCode.LeftArrow)) { x -= 1; }
-        if(UnityEngine.Input.GetKey(KeyCode.RightArrow)) { x += 1; }
-        if(UnityEngine.Input.GetKey(KeyCode.UpArrow)) { y += 1; }
-        if(UnityEngine.Input.GetKey(KeyCode.DownArrow)) { y -= 1; }
+        if(UnityEngine.Input.GetKey(KeyCode.LeftArrow)) { vec.x -= 1; }
+        if(UnityEngine.Input.GetKey(KeyCode.RightArrow)) { vec.x += 1; }
+        if(UnityEngine.Input.GetKey(KeyCode.UpArrow)) { vec.y += 1; }
+        if(UnityEngine.Input.GetKey(KeyCode.DownArrow)) { vec.y -= 1; }
 
-        var angle = math.atan2(y, x);
-        var magnitude = math.sqrt(math.pow(x, 2) + math.pow(y, 2));
-
-        var vec = new float2(math.cos(angle) * magnitude,
-                             math.sin(angle) * magnitude);
+        if(math.length(vec) > 0f) {
+            vec = math.normalize(vec);
+        }
 
         Entities.ForEach((ref Input input) =>
         {
